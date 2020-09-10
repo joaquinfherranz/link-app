b57687bc22f2d81b2e5f8e86d050811040c2577b
joaquinfherranz <joaquinfherranz@gmail.com>
2020-09-10 20:03:44 +0200

    Add server API root

--- __test__/services/server.test.js
+++ __test__/services/server.test.js
@@ -1,15 +1,14 @@
 const request = require('supertest')
 const app = require('../../services/server')
 
-describe('GET / endpoint', () => {
+describe('GET /api/v1 endpoint', () => {
   test('Should respond with a Hello World!', () => {
     request(app)
-      .get('/')
+      .get('/api/v1')
       .expect('Content-Type', /json/)
       .expect(200)
       .then(({ body }) => {
         expect(body.message).toBe('Hello World!')
       })
-      .catch(console.error)
   })
 })
--- services/server.js
+++ services/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/api/v1/', (req, res) => {
   res.json({ message: 'Hello World!' })
 })
 
