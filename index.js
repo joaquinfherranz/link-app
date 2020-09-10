0f99ea7e168d2533f4dcdef78aaa95a8b60ed540
joaquinfherranz <joaquinfherranz@gmail.com>
2020-09-10 22:49:13 +0200

    Add https
     - Handle requests for the data
     - Handle requests for static files

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config()
 
 const app = require('./services/server')
 const port = process.env.PORT
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
+
+// Redirect HTTP to HTTPS,
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- services/server.js
+++ services/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 
+// Handle requests for the data
 app.get('/api/v1/', (req, res) => {
   res.json({ message: 'Hello World!' })
 })
+
+// Handle requests for static files
+app.use(express.static('public'))
 
 module.exports = app
