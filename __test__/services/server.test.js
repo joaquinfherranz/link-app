5c9408000ff7007f124396ecd486adb324133030
joaquinfherranz <joaquinfherranz@gmail.com>
2020-09-11 00:08:01 +0200

    Add a static file sample test

--- __test__/services/server.test.js
+++ __test__/services/server.test.js
@@ -2,13 +2,25 @@ const request = require('supertest')
 const app = require('../../services/server')
 
 describe('GET /api/v1 endpoint', () => {
-  test('Should respond with a Hello World!', () => {
+  test('Should respond a json with a Hello World!', () => {
     request(app)
       .get('/api/v1')
       .expect('Content-Type', /json/)
       .expect(200)
       .then(({ body }) => {
         expect(body.message).toBe('Hello World!')
+      })
+  })
+})
+
+describe('GET / endpoint', () => {
+  test('Should respond an html file that contains Hello developers!', async () => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', /html/)
+      .then((res) => {
+        expect(res.text).toContain('Hello developers!')
       })
   })
 })
