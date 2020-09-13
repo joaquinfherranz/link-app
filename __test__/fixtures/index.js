017962c4e163f34ad20444aee3a5f5b7481fec10
Joaquin F. Herranz <joaquinfherranz@gmail.com>
2020-09-14 00:56:26 +0200

    Add fixtures for freecodecamp downloader

--- __test__/fixtures/index.js
+++ __test__/fixtures/index.js
@@ -1,9 +1,14 @@
 const news = require('./samples/news.json')
+const { readFileSync } = require('fs')
+const { join } = require('path')
 
 module.exports = {
   news: {
     allNews: news,
     newsDetail: news[0],
     validId: news[0].id
+  },
+  sources: {
+    freecodecamp: readFileSync(join(__dirname, './sources_samples/freecodecamp.html'), 'utf8')
   }
 }
