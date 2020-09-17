8a2c78073c6d64d4e498caa57ea8722ceee6f64b
Joaquin F. Herranz <joaquinfherranz@gmail.com>
2020-09-17 10:34:06 +0200

    Add metaescraper
     - Refactor got mock
     - Include request to clearbit url called by metascraper-clearbit

--- __mocks__/got.js
+++ __mocks__/got.js
@@ -2,7 +2,12 @@ const { sources } = require('../__test__/fixtures')
 
 const responseBody = {
   'https://www.freecodecamp.org/news/': sources.freecodecamp,
-  'https://javascriptkicks.com/stories': sources.javascriptkicks
+  'https://javascriptkicks.com/stories': sources.javascriptkicks,
+  'https://web.dev/promises/': sources.promises,
+  'https://autocomplete.clearbit.com/v1/companies/suggest': sources.clearbits
 }
 
-module.exports = (url) => responseBody[url] ? Promise.resolve({ body: responseBody[url] }) : Promise.reject(new Error('unknown url'))
+module.exports = (url) =>
+  responseBody[url]
+    ? Promise.resolve({ body: responseBody[url], url })
+    : Promise.reject(new Error('unknown url'))
--- services/metascraper.js
+++ services/metascraper.js
@@ -1,2 +1,19 @@
+// @see https://github.com/microlinkhq/metascraper
+const metascraper = require('metascraper')([
+  require('metascraper-author')(),
+  require('metascraper-date')(),
+  require('metascraper-description')(),
+  require('metascraper-image')(),
+  require('metascraper-logo')(),
+  require('metascraper-clearbit')(),
+  require('metascraper-publisher')(),
+  require('metascraper-title')(),
+  require('metascraper-url')()
+])
+const got = require('got')
 
-module.exports = (url) => {}
+module.exports = async (targetUrl) => {
+  const { body: html, url } = await got(targetUrl)
+  const metadata = await metascraper({ html, url })
+  return metadata
+}
