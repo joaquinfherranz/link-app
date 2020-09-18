3025f7782362ee29f7b09e5460fa565a03be3887
Joaquin F. Herranz <joaquinfherranz@gmail.com>
2020-09-18 15:22:04 +0200

    Start the app reading the news

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 const app = require('./services/server')
 const port = process.env.PORT
 const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
+const { news: newsTask } = require('./tasks')
+const { reset } = require('./services/store')
+
 // const { resetDB, populateDB } = require('./__test__/helpers')
 
 // Redirect HTTP to HTTPS,
@@ -12,6 +15,13 @@ resetDB()
 populateDB()
 */
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const init = async () => {
+  reset()
+  console.log('Reading news ...')
+  await newsTask()
+  app.listen(port, () => {
+    console.log(`Link app listening at http://localhost:${port}`)
+  })
+}
+
+init()
